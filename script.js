df87351c64e5a0191036beafb049c8ecc5decbc1
Serge Davidov <sd@kreativkombinat.de>
2013-01-12 10:47:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+		$('.home a').hover(function(){
+	         	$(this).stop().animate({'opacity' : '0'}, 500);
+	 	}, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+	
+         $('.heuer a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+		$('.ploschka a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+		$('.bergerhausen a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+		$('.hestermann a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+
+		$('.kriechbaumer a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+		
+		 $('.lauf a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+
+		$('.kraft a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+
+		$('.funktionelles a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+
+		$('.gewicht a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+
+		$('.ernaehrung a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+		$('.schwimm a').hover(function(){
+		         $(this).stop().animate({'opacity' : '0'}, 500);
+		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
+		
+});
